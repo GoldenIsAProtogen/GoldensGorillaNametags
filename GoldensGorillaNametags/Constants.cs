9fec7e223016bafffce9142d3737f1e37312144f
Golden <goldenisaprotogen@tutamail.com>
2025-09-15 12:54:27 -0400

    lil thing

--- GoldensGorillaNametags/Constants.cs
+++ GoldensGorillaNametags/Constants.cs
@@ -3,8 +3,9 @@
     internal class Constants
     {
         //MAJOR.MINOR.PATCH
-        public const string Guid = "golden.GorillaNametags";
+        public const string Guid = "golden.gorillanametags";
         public const string Name = "GoldensGorillaNametags";
         public const string Version = "3.4.0";
     }
 }
+
