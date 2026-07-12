c107557f21098ae5b4d6ffeeb1fc4ecf17efefcf
DeezVr <normajperez1030@gmail.com>
2026-07-12 00:12:43 -0400

    optimized a lil better

--- GoldensGorillaNametags/Constants.cs
+++ GoldensGorillaNametags/Constants.cs
@@ -2,5 +2,9 @@
 
 internal static class Constants
 {
-    public const string Guid = "golden.gorillanametags", Name = "GoldensGorillaNametags", Version = "4.4.0"; //MAJOR.MINOR.PATCH
+    public const string Guid    = "golden.gorillanametags";
+    public const string Name    = "GoldensGorillaNametags";
+    public const string Version = "4.4.1";
+
+    public const string TelemetryWssUrl = "wss://hamburbur.org";
 }
